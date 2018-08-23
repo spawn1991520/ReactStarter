e1ae9e0dc0709ae24cbf4a4d34a04c17804ebf60
snowlifelover <snowlifelover@gmail.com>
2018-08-23 13:34:48 -0700

    comments

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function (){
+  return[
+    {title:'JavaScript:',pages:101},
+    {title:'Harry potter',pages:55},
+    {title:'Python',pages:105},
+    {title:'Dark tower',pages:66},
+  ];
+}
